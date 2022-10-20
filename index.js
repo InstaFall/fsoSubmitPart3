467fa966400ed7962349a3205c3d3daa4d66fbc8
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-20 23:54:48 +0300

    Commit exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(morgan('tiny'))
 const PORT = 3001
 
 let persons = [
@@ -81,6 +91,12 @@ app.post("/api/persons", (req, res) => {
   console.log(newPerson)
   res.json(newPerson)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
