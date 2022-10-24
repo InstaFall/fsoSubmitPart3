cad77f4ee0596a7676735a1364798ea51cf51b0b
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-25 01:33:21 +0300

    Commit exercise 3.16

--- index.js
+++ index.js
@@ -2,20 +2,14 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-const cors = require('cors') 
+const cors = require('cors')
 const Person = require('./models/person')
-
 //app.use(cors())
-const errHandler = (error, request, response,next) =>{
-  if (error.name === "CastError"){
-    return response.status(400).send({error: 'malformatted id'})
-  }
-  next(error)
-}
+
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('info',(req,res) => JSON.stringify(req.body))
+morgan.token('info', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :info'))
 
 
@@ -23,7 +17,7 @@ app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons)
   })
-}) 
+})
 
 app.get("/info", (req, res) => {
   const date = new Date().toString()
@@ -32,7 +26,7 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id).then((person) => {
-    if(person){
+    if (person) {
       res.json(person)
     } else {
       res.status(404).end()
@@ -42,10 +36,10 @@ app.get("/api/persons/:id", (req, res, next) => {
 
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-  .then(response => {
-    res.status(204).end()
-  })
-  .catch(err => next(err))
+    .then(response => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
 app.post("/api/persons", (req, res) => {
@@ -55,13 +49,13 @@ app.post("/api/persons", (req, res) => {
       error: "content-missing"
     })
   }
-  Person.find({name: body.name}).then(person => {
-    if (person.some(el => el.name === body.name)) { 
+  Person.find({ name: body.name }).then(person => {
+    if (person.some(el => el.name === body.name)) {
       return res.status(409).json({
         error: "duplicate-entry"
       })
     }
-    else{
+    else {
       const newPerson = new Person({
         name: body.name,
         number: body.number
@@ -79,7 +73,14 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
 app.use(errHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
