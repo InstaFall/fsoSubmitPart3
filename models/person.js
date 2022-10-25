ba3427467aa5fe150e32d0011f8be68d70379380
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-25 18:13:48 +0300

    Commit exercise 3.20

--- models/person.js
+++ models/person.js
@@ -16,7 +16,26 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (num) => {
+        if (num.includes('-')){  
+          const splitNumber = num.split('-')
+          if (splitNumber.length > 2){
+            return false
+          } else {
+            if(splitNumber[0].length > 3 || splitNumber[0].length === 1) {
+              return false
+            }
+            if(isNaN(splitNumber[1])) {
+              return false
+            }
+          }
+        } else return false
+      },
+      message: 'Not a valid number! Number Pattern: xxx-xxxxxxxx'
+    }
   }
 })
 
